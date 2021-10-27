a15dd8c1dc0ea7a967a3bf4386bcd580929d9bdf
niato <j1821@iot-21.epi.it.matsue-ct.jp>
2021-10-27 11:16:35 +0900

    ch-2

--- kadai1.rb
+++ kadai1.rb
@@ -1,11 +1,20 @@
-for (i = 1; i <= 100; i++) {
-  if (i % 3 == 0 && i % 5 == 0){
+i=1
+
+while (i<=100) do
+  if (i % 3 == 0 && i % 5 == 0)
     printf("FizzBuzz\n");
-  } else if (i % 3 == 0) {
-    printf("Fizz\n");
-   } else if (i % 5 == 0) {
-    printf("Buzz\n");
-  } else {
-    printf("%d\n", i);
-  }
-}
+  
+  elsif (i % 3 == 0)
+    puts("Fizz");
+  
+  elsif (i % 5 == 0) 
+    puts("Buzz");
+  
+  else {
+    puts("%d", i);
+  
+  end
+
+  i = i + 1
+
+end
